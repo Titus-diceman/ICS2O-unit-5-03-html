28b887f217d1af9e6856114d0ff93f7706235004
Titus-diceman <99354708+Titus-diceman@users.noreply.github.com>
2022-04-28 17:04:44 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/* Created by: Titus Diceman
+   Created on: April 2022
+   This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check servie worker.
+ */
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-unit-5-03-html/sw.js", {
+    scope: "/ICS2O-unit-5-03-html/",
+  })
+}
+
+/**
+ * This function displays an xxx.
+ */
+function buttonClicked() {
+
+  var over17 = document.getElementById
+  var over17 = document.getElementById 
+  var over17 = document.getElementById
+
+
+if (over17.checked >= "red") {
+  document.getElementById("age")inner.html
+} else if (colourOfLight == "yellow") {
+document.getElementById("age")inner.html
+} else if (colourOfLight == "green") {
+ document.getElementById("age")inner.html 
+} else {
+ 
+}
